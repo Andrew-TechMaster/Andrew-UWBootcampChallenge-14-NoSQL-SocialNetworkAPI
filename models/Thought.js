e3e8e00d522a8443c298888610aa3d56b66eb2c2
Andrew Chou <andrewchou1996@gmail.com>
2023-03-21 10:05:03 -0700

    Finish models

--- models/Thought.js
+++ models/Thought.js
@@ -1,8 +1,99 @@
 const { Schema, model } = require("mongoose");
 
-// Schema to create thought model
-const thoughtSchema = new Schema({}, {});
+/**
+ * Helper func to format the date
+ *
+ * @param {Date} d The date to be formatted.
+ * @return {string} datestring, Month/Day/Year - H:M.
+ */
+function dateFormatter(d) {
+  var datestring = `${
+    d.getMonth() + 1
+  }/${d.getDate()}/${d.getFullYear()} - ${d.getHours()}:${d.getMinutes()}`;
 
+  return datestring;
+}
+
+/**
+ * Schema to create Reaction model
+ * This will not be a model, but rather will be used as the reaction field's subdocument schema in the Thought model.
+ */
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: dateFormatter,
+    },
+  },
+  {
+    toJSON: {
+      // To run getters when converting a document to JSON, set the toJSON.getters option to true in your schema
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+/**
+ * Schema to create Thought model
+ */
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+      get: dateFormatter,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+    toJSON: {
+      // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+      virtuals: true,
+      // To run getters when converting a document to JSON, set the toJSON.getters option to true in your schema
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+/**
+ * Create a virtual property `reactionCount`
+ * that retrieves the length of the thought's reactions array field on query.
+ */
+thoughtSchema
+  .virtual("reactionCount")
+  // Getter
+  .get(() => this.reactions.length);
+
+/**
+ * Initialize our Thought model
+ */
 const Thought = model("thought", thoughtSchema);
 
 module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,8 +1,60 @@
 const { Schema, model } = require("mongoose");
 
-// Schema to create a user model
-const userSchema = new Schema({}, {});
+/**
+ * Schema to create a User model
+ */
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+      virtuals: true,
+    },
+    // If you don't want an id getter added to your schema, you may disable it by passing this option at schema construction time.
+    id: false,
+  }
+);
 
+/**
+ * Create a virtual property `friendCount`
+ * that retrieves the length of the user's `friends` array field on query.
+ */
+userSchema
+  .virtual("friendCount")
+  // Getter
+  .get(() => this.friends.length);
+
+/**
+ * Initialize our User model
+ */
 const User = model("user", userSchema);
 
 module.exports = User;
